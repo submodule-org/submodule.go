390bd099eadcaf2d294598b60ede0a9b7fc61f56
Duke <duc@silentium.io>
2024-05-02 03:55:35 +0000

    test: added resolvers test

---
+++ resolvers_test.go
@@ -0,0 +1,62 @@
+package submodule
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var intValue = Make[int](func() int {
+	return 100
+})
+
+var stringValue = Make[string](func() string {
+	return "hello"
+})
+
+type Embedded struct {
+	In
+	Int    int
+	String string
+}
+
+func TestResolver(t *testing.T) {
+	t.Run("can resolve type", func(t *testing.T) {
+		store := CreateStore()
+		r, e := resolveType(store, reflect.TypeOf(0), []Retrievable{intValue})
+		assert.Nil(t, e)
+		assert.Equal(t, r.Interface(), 100)
+
+		r, e = resolveType(
+			store,
+			reflect.TypeOf(Embedded{}),
+			[]Retrievable{intValue, stringValue},
+		)
+
+		v, ok := r.Interface().(Embedded)
+		assert.True(t, ok)
+		assert.Nil(t, e)
+		assert.Equal(t, 100, v.Int)
+		assert.Equal(t, "hello", v.String)
+	})
+
+	t.Run("can resolve embedded type", func(t *testing.T) {
+		store := CreateStore()
+
+		var v Embedded
+
+		_, e := resolveEmbedded(
+			store,
+			reflect.TypeOf(Embedded{}),
+			reflect.ValueOf(&v),
+			[]Retrievable{intValue, stringValue},
+		)
+
+		assert.Nil(t, e)
+
+		assert.Equal(t, v.Int, 100)
+		assert.Equal(t, v.String, "hello")
+	})
+
+}
